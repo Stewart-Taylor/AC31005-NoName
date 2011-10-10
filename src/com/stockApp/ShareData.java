3c178d087470260bd4777f53528ed1140a9ab381
Taylore <stewart.taylor1@gmail.com>
2011-10-10 16:38:11 +0200

    Added basic structure for save shares data system.

--- src/com/stockApp/ShareData.java
+++ src/com/stockApp/ShareData.java
@@ -3,10 +3,48 @@ package com.stockApp;
 public class ShareData 
 {
 
+
+	
+ ArrayList<Share> shares = new ArrayList<Share>();
+
+public struct Share
+{
+public int price;
+public String stockCode;
+public int lastPrice;
+public int amount;
+}
+
+
 	
 	
 	
-	
-	
+public ArrayList<Share> getShares()
+{
+return shares;
+}
+
+
+
+
+private void loadShareData()
+{
+
+
+
+
+}
+
+
+
+
+private void saveShareData()
+{
+
+
+
+
+}
+
 	
 }
