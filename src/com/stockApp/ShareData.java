063544e2779a5e4ad0f068644681c46cbf139696
Stewart <stewart.taylor1@gmail.com>
2011-10-17 13:26:11 +0200

    Added new Bowleven share set , but still requires verified amount!

--- src/com/stockApp/ShareData.java
+++ src/com/stockApp/ShareData.java
@@ -43,6 +43,7 @@ private void loadShareData()
 	 shares.add( new Share( "Experian ordinary" , "EXPN", 258));
 	 shares.add( new Share( "Marks and Spencer Ordinary 25" , "MKS", 485));
 	 shares.add( new Share( "Smith & Nephew plc ordinary" , "SN.", 1219));
+	 shares.add( new Share( "Bowleven plc  ord 10P" , "BLVN", 100));  //AMOUNTS NEEDS CONFIRMED
  	
 
 
