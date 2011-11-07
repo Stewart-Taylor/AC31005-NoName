42a5e5c41509de119be6aaddd3e0f120f3c08a7b
Stewart <stewart.taylor1@gmail.com>
2011-11-07 12:01:46 +0000

    Code Refactoring

--- src/com/stockApp/AboutActivity.java
+++ src/com/stockApp/AboutActivity.java
@@ -1,14 +1,12 @@
 package com.stockApp;
 
 import android.app.Activity;
-
 import android.os.Bundle;
-
-
 
 
 public class AboutActivity extends Activity 
 {
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -19,16 +17,7 @@ public class AboutActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         
+        //Activity just displays strings from about xml file and strings xml
         
-        
-       
-        
-        
-        
-    }
-    
-    
-  
-    
-    
+    } 
 }
