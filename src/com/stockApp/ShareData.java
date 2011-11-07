137711bda38a720741295e688eae4eaa5f34cd93
Stewart <stewart.taylor1@gmail.com>
2011-11-07 12:51:39 +0000

    Code Refactoring

--- src/com/stockApp/ShareData.java
+++ src/com/stockApp/ShareData.java
@@ -3,63 +3,46 @@ package com.stockApp;
 
 import java.util.ArrayList;
 
+
 public class ShareData 
 {
 
-
-	
- ArrayList<Share> shares = new ArrayList<Share>();
-
-
-
+	private ArrayList<Share> shares = new ArrayList<Share>();
 	
 	
-	
- 
- 
-
-public ArrayList<Share> getShares()
-{
-return shares;
-}
-
-
-
- public ShareData()
-{
-	loadShareData();
+	public ArrayList<Share> getShares()
+	{
+		return shares;
+	}
 	
 	
-}
-
-
-private void loadShareData()
-{
-
-	//will get from file in future
+	public ShareData()
+	{
+		loadShareData();	
+	}
 	
-	 shares.add( new Share( "BP Amoco pl" , "BP", 192));
-	 shares.add( new Share( "HSBC Holdings plc ordinary 50 Cents" , "HSBA", 343));
-	 shares.add( new Share( "Experian ordinary" , "EXPN", 258));
-	 shares.add( new Share( "Marks and Spencer Ordinary 25" , "MKS", 485));
-	 shares.add( new Share( "Smith & Nephew plc ordinary" , "SN.", 1219));
-	 shares.add( new Share( "Bowleven plc  ord 10P" , "BLVN", 3960));  
- 	
+	
+	private void loadShareData()
+	{
 
+		//will get from file in future
+		 shares.add( new Share( "BP Amoco pl" , "BP", 192));
+		 shares.add( new Share( "HSBC Holdings plc ordinary 50 Cents" , "HSBA", 343));
+		 shares.add( new Share( "Experian ordinary" , "EXPN", 258));
+		 shares.add( new Share( "Marks and Spencer Ordinary 25" , "MKS", 485));
+		 shares.add( new Share( "Smith & Nephew plc ordinary" , "SN.", 1219));
+		 shares.add( new Share( "Bowleven plc  ord 10P" , "BLVN", 3960));  
+	}
+	
+	
+	
+	
+	private void saveShareData()
+	{
+	
+		//yeah i know...
 
-
-}
-
-
-
-
-private void saveShareData()
-{
-
-
-
-
-}
+	}
 
 	
 }
