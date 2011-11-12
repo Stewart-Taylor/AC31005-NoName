eeb8e193e4fac5969a58ee13ce1eceba08cbd16b
Lyuben T <ltodorov@dundee.ac.uk>
2011-11-12 04:51:23 +0000

    updated comments in MainActivity clarifying how the class works]

--- workspace/StockMarket/src/com/stockApp/MainActivity.java
+++ workspace/StockMarket/src/com/stockApp/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+    /** Called when the app is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
