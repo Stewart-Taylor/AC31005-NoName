e7ba3ae0018d8e51f183ea8ccbab80b473bb9164
Lyuben T <ltodorov@dundee.ac.uk>
2011-11-18 11:51:18 +0000

    removed -yeah i know...- method

---
+++ src/com/testStock/ShareTest.java
@@ -0,0 +1,27 @@
+package com.testStock;
+
+import junit.framework.TestCase;
+
+public class ShareTest extends TestCase {
+
+	public void testShare() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetShareName() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStockCode() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetOldPrice() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetShareAmount() {
+		fail("Not yet implemented");
+	}
+
+}
